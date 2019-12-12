784965f5068d95fb7432c66cc1f5bc5cf5a5f8cc
Fernanda Schaefer <schaefernanda@gmail.com>
2019-12-12 16:17:32 -0600

    Generate first version of the batch upload CSV.

--- fschaef2_final_project.py
+++ fschaef2_final_project.py
@@ -49,29 +49,59 @@ for record in UIUC_patents:
     if record not in unique_patents:
         unique_patents.append(record)
 
-PDF_URLs = []
-
-def get_patent_PDFs():
-    target = pathlib.Path('fschaef2_patent_PDFs')
-    pbar = ProgressBar()
-    for item in pbar(unique_patents):
-        patent_number = item['patent_number']
-        first_portion = patent_number[-2:]
-        second_portion = patent_number[2:5]
-        third_portion = str(0) + patent_number[:2]
-        url = "http://pimg-fpiw.uspto.gov/fdd/" + str(first_portion) + "/" + str(second_portion) + "/" + third_portion + "/0.pdf"
-        PDF_URLs.append(url)
-        myfile = requests.get(url)
-        filename = patent_number + ".pdf"
-        p = str(target / filename)
-        with open(p, 'wb') as file:
-            file.write(myfile.content)
-
-get_patent_PDFs()
+# def get_patent_PDFs():
+#     target = pathlib.Path('fschaef2_patent_PDFs')
+#     pbar = ProgressBar()
+#     for item in pbar(unique_patents):
+#         patent_number = item['patent_number']
+#         first_portion = patent_number[-2:]
+#         second_portion = patent_number[2:5]
+#         third_portion = str(0) + patent_number[:2]
+#         url = "http://pimg-fpiw.uspto.gov/fdd/" + str(first_portion) + "/" + str(second_portion) + "/" + third_portion + "/0.pdf"
+#         myfile = requests.get(url)
+#         filename = patent_number + ".pdf"
+#         p = str(target / filename)
+#         with open(p, 'wb') as file:
+#             file.write(myfile.content)
+#
+# get_patent_PDFs()
 
 # with open('fschaef2_UIUC_patents.json', 'w') as file_out:
 #     json.dump(unique_patents, file_out, indent=4)
 
+def generate_IDEALS_batch_upload_csv():
 
+    for patent_information in unique_patents:
+            dc_creator = []
+            inventors = patent_information['inventors']
+            for inventor_information in inventors:
+                inventor_last_name = inventor_information['inventor_last_name']
+                inventor_first_name = inventor_information['inventor_first_name']
+                IDEALS_creator = str(inventor_last_name) + ", " + str(inventor_first_name)
+                dc_creator.append(IDEALS_creator)
+
+    allrows = []
+
+    for record in unique_patents:
+        row = []
+        BUNDLE_ORIGINAL = record['patent_number'] + ".pdf"
+        dc_title = record['patent_title']
+        dc_date_issued = record['patent_date']
+        dc_description_abstract = record['patent_abstract']
+        dc_identifier = record['patent_number']
+        row.append(BUNDLE_ORIGINAL)
+        row.append(dc_title)
+        row.append(dc_creator)
+        row.append(dc_date_issued)
+        row.append(dc_description_abstract)
+        row.append(dc_identifier)
+        allrows.append(row)
+
+    outfile = open('fschaef2_IDEALS_batch_upload.csv', 'w')
+    csvout = csv.writer(outfile)
+    csvout.writerow(['BUNDLE:ORIGINAL', 'dc_title', 'dc_creator', 'dc_date_issued', 'dc_description_abstract', 'dc_identifier'])
+    csvout.writerows(allrows)
+
+generate_IDEALS_batch_upload_csv()
 
 
